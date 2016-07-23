91677f6dd4c6ce55a1493368877cdf80713fc57d
Lawrence Allan Jones <mrwizard82d1@gmail.com>
2016-07-23 16:34:11 -0500

    Initial (empty) browser window

--- bookmarker/app/main.js
+++ bookmarker/app/main.js
@@ -1,6 +1,12 @@
-const {app} = require('electron');
+const {app, BrowserWindow} = require('electron');
+
+// Must allocate `mainWindow` **outside** the 'ready' handler; otherwise, when the browser runs this script,
+// `mainWindow` will be eligible for garbage collection **immediately**. Because different implementations of the
+// garbage collector will have different behavior, failing to allocate the variable outside the function will sometimes
+// work and sometimes not.
+mainWindow = null;
 
 app.on('ready', () => {
 	console.log('Hello from Electron.');
+	mainWindow = new BrowserWindow();
 });
-
