0e36ba5fab17eeea09b0c10260f38d2b2a354d20
Larry Jones <larryj@blinds.com>
2017-12-29 17:19:35 -0600

    Handle new URL submission

--- bookmarker/app/renderer.js
+++ bookmarker/app/renderer.js
@@ -13,3 +13,21 @@ const clearForm = () => {
   newLinkUrl.value = null;
 };
 
+function appendMessage(text) {
+  document.querySelector('#messages').value += `\n${text.toString()}`;
+}
+
+function handleSubmit(event) {
+  appendMessage('In `handleSubmit`');
+  event.preventDefault();
+  
+  const newUrl = newLinkUrl.value;
+  appendMessage(`newUrl === ${newUrl}`);
+  
+  fetch(newUrl)
+    .then(response => response.text())
+    .then(text => appendMessage((_.toJs(_.subvec(_.seq(text), 0, 100))).join('')))
+    .catch(e => appendMessage(`Error, ${e}, fetching url, ${newUrl} )`));
+}
+
+newLinkForm.addEventListener('submit', handleSubmit);
