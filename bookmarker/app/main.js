fb8ea4e66d2173a676c4557acc0576857a98050b
Larry Jones <larryj@blinds.com>
2017-12-29 01:34:33 -0600

    Render (an empty) window

--- bookmarker/app/main.js
+++ bookmarker/app/main.js
@@ -1,5 +1,10 @@
-const { app } = require('electron');
+const { app, BrowserWindow } = require('electron');
+
+// Define `mainWindow` outside `app.on` callback so it does not go out of scope before we try to
+// render it.
+let mainWindow = null;
 
 app.on('ready', () => {
   console.log('Hello from Electron!');
+  mainWindow = new BrowserWindow();
 });
