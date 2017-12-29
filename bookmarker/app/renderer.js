cdd51611352d332a9b1065baf844dff515184b8e
Larry Jones <larryj@blinds.com>
2017-12-29 02:38:20 -0600

    Enable submit button on valid URL

--- bookmarker/app/renderer.js
+++ bookmarker/app/renderer.js
@@ -1,2 +1,15 @@
-const button = document.querySelector('.alert');
-button.addEventListener('click', () => alert(__dirname));
+const linksSection = document.querySelector('.links');
+const errorMessage = document.querySelector('.error-message');
+const newLinkForm = document.querySelector('.new-link-form');
+const newLinkUrl = document.querySelector('.new-link-url');
+const newLinkSubmit = document.querySelector('.new-link-submit');
+const clearStorageButton = document.querySelector('.clear-storage');
+
+newLinkUrl.addEventListener('keyup', () => {
+  newLinkSubmit.disabled = !newLinkUrl.validity.valid;
+});
+
+const clearForm = () => {
+  newLinkUrl.value = null;
+};
+
