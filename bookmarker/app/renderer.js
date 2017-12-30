9c8387c3477de7d65e87db7725d9863cfa73aad3
Larry Jones <larryj@blinds.com>
2017-12-29 18:36:48 -0600

    Store links in local storage

--- bookmarker/app/renderer.js
+++ bookmarker/app/renderer.js
@@ -27,16 +27,33 @@ function findTitle(domNodes) {
   return domNodes.querySelector('title').innerText;
 }
 
+function storeUrl(title, url) {
+  localStorage.setItem(url, JSON.stringify({
+    title,
+    url,
+  }));
+}
+
+function dumpLocalStorage() {
+  for(var i = 0; i < localStorage.length; i++) {
+    const url = localStorage.key(i);
+    const title = JSON.parse(localStorage.getItem(url)).title;
+    appendMessage(`${url}: ${title}`);
+  }
+}
+
 function handleSubmit(event) {
-  appendMessage('In `handleSubmit`');
   event.preventDefault();
   
   const newUrl = newLinkUrl.value;
-  appendMessage(`newUrl === ${newUrl}`);
   
   fetch(newUrl)
     .then(response => response.text())
-    .then(text => _.pipeline(text, parseResponse, findTitle, appendMessage))
+    .then(text => {
+      _.pipeline(text, parseResponse, findTitle, _.curry(storeUrl, newUrl));
+      clearForm();
+      dumpLocalStorage();
+    })
     .catch(e => appendMessage(`Error, ${e}, fetching url, ${newUrl} )`));
 }
 
